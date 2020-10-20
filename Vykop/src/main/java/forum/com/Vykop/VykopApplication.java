9dbbbdc7942ff364c09f96ae60244ccff5afc023
Trunio <kondrattrunowicz@wp.pl>
2020-10-20 17:39:49 +0200

    Spring Initializer - Templatka

---
+++ Vykop/src/main/java/forum/com/Vykop/VykopApplication.java
@@ -0,0 +1,13 @@
+package forum.com.Vykop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VykopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VykopApplication.class, args);
+	}
+
+}
---
+++ Vykop/src/test/java/forum/com/Vykop/VykopApplicationTests.java
@@ -0,0 +1,13 @@
+package forum.com.Vykop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VykopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
